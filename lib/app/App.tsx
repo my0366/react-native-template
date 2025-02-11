ff9cdc22e7e5454151940bf55c83377fbd305bcd
ellen-musma <ellen@musma.net>
2025-02-11 15:46:41 +0900

    init queryClient

--- lib/app/App.tsx
+++ lib/app/App.tsx
@@ -5,15 +5,18 @@
  * @format
  */
 
-import {View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import RootStackNavigator from './routes.tsx';
+import {QueryClientProvider} from '@tanstack/react-query';
+import {queryClient} from './queryClient.ts';
 
 function App() {
   return (
-    <NavigationContainer>
-      <RootStackNavigator />
-    </NavigationContainer>
+    <QueryClientProvider client={queryClient}>
+      <NavigationContainer>
+        <RootStackNavigator />
+      </NavigationContainer>
+    </QueryClientProvider>
   );
 }
 
